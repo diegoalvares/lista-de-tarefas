7c1b3239cf7a97a2d85250a999448917f2e797f4
Diego Alvares de Macedo <101260437+diegoalvares@users.noreply.github.com>
2025-10-16 09:51:35 -0300

    projeto finalizado, faltando apenas adiconar efeitos.

--- JS/script.js
+++ JS/script.js
@@ -6,6 +6,8 @@ nome_tarefa = document.getElementById('nome_tarefa');
 descricao = document.getElementById('descricao');
 rodape = document.getElementById('rodape');
 
+    //---------------------------------------------//
+
 
 ntrf.addEventListener('click', function novatarefa(){
     formulario.style.display = 'block';
@@ -15,14 +17,33 @@ ntrf.addEventListener('click', function novatarefa(){
 
 adc.addEventListener('click', function adicionar(e){
     e.preventDefault();
+    
+//---------------------------------------------//
     const card = document.createElement("div");
-    card.classList.add('card');
     const titulo_card = document.createElement("h3");
     const linha = document.createElement("hr");
     const titulo_card2 = document.createElement("h4");
     const pg_card = document.createElement("p");
 
     //---------------------------------------------//
+    const concluir = document.createElement('button');
+    concluir.innerText= 'Concluir';
+    concluir.classList.add('concluir');
+    
+    concluir.addEventListener('click', function novatarefa(){
+    alert('Parabéns por concluir a tarefa!')
+        card.remove();
+    })
+
+    const apagar = document.createElement('button');
+    apagar.innerText= 'X Deletar'
+    apagar.classList.add('apagar');
+
+    apagar.addEventListener('click', function novatarefa(){
+    alert('Tarefa deletada!')
+        card.remove();
+    })
+  //---------------------------------------------//
 
     titulo_card.innerText= nome_tarefa.value;
     titulo_card2.innerText='Descrição da Tarefa';
@@ -30,16 +51,36 @@ adc.addEventListener('click', function adicionar(e){
 
     //---------------------------------------------//
 
-    document.body.appendChild(card);
-    card.appendChild(titulo_card);
-    card.appendChild(linha);
-    card.appendChild(titulo_card2);
-    card.appendChild(pg_card);
-    rodape.parentNode.insertBefore(card, rodape);
+    if (nome_tarefa.value == ''){
+        alert('Coloque ao menos o nome da tarefa.')
+    }
 
-    formulario.style.display='none';
+    else if (nome_tarefa.value != '' && descricao.value == ''){
+        document.body.appendChild(card);
+        card.classList.add('card2');
+        card.appendChild(titulo_card);
+        card.appendChild(concluir);
+        card.appendChild(apagar);
+        rodape.parentNode.insertBefore(card, rodape);
+        formulario.style.display='none';
+    }
+
+    else if (nome_tarefa.value != ''){
+        document.body.appendChild(card);
+        card.classList.add('card');
+        card.appendChild(titulo_card);
+        card.appendChild(linha);
+        card.appendChild(titulo_card2);
+        card.appendChild(pg_card);
+        card.appendChild(concluir);
+        card.appendChild(apagar);
+        rodape.parentNode.insertBefore(card, rodape);
+        formulario.style.display='none';
+    }
+   
 })
 
+    //---------------------------------------------//
 
 cnc.addEventListener('click', function cancelar(){
     formulario.style.display = 'none';
